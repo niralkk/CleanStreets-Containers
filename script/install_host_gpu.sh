6c24d90c43f2d3f877e4d6da37caf7674695fce9
Tsung Min Huang <tsungmin.huang@sjsu.edu>
2017-10-12 09:59:11 -0700

    install host gpu script clean up

--- script/install_host_gpu.sh
+++ script/install_host_gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Install CUDA8.0 support for Host\n"
+echo "Install CUDA8.0 support for Host"
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 sudo apt-get update
@@ -8,8 +8,6 @@ sudo apt-get install cuda-8-0
 
 # clean up
 sudo rm cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-sudo apt-get 
 
 echo "Install CUDA8.0 done\n"
-echo "\n========= GPU info ==========\n"
 nvidia-smi
