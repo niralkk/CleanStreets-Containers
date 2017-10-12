2abd95e7f3042b34d5f00a965c1c8b178117df52
Tsung Min Huang <tsungmin.huang@sjsu.edu>
2017-10-12 09:51:34 -0700

    add clean step after install host gpu

--- script/install_host_gpu.sh
+++ script/install_host_gpu.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-echo "Install CUDA8.0 support for Host"
+echo "Install CUDA8.0 support for Host\n"
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 sudo apt-get update
 sudo apt-get install cuda-8-0
 
-echo "Install CUDA8.0 done"
-echo "GPU info"
+# clean up
+sudo rm cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+sudo apt-get 
+
+echo "Install CUDA8.0 done\n"
+echo "\n========= GPU info ==========\n"
 nvidia-smi
