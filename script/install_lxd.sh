100c0367a78be1a03235bf38bfe64400f754473d
Tsung Min Huang <tsungmin.huang@sjsu.edu>
2017-10-12 10:10:48 -0700

    fix lxd init step failed

--- script/install_lxd.sh
+++ script/install_lxd.sh
@@ -18,20 +18,25 @@ CONTAINER=cuda
 EXPORT_NAME=ub1604_cuda8
 
 echo "Build LXC GPU image"
-sudo lxc init --auto
+sudo lxc init --auto \
+    --storage-backend dir \
+    --network-address 0.0.0.0
+    --network-port 8443
+
 lxc launch ubuntu:16.04 $CONTAINER
 lxc exec $CONTAINER -- wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 lxc exec $CONTAINER -- dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 lxc exec $CONTAINER -- apt-get update
 lxc exec $CONTAINER -- apt-get install cuda-demo-suite-8-0 --no-install-recommends
-echo 'LXC GPU image cuda install done'
+echo 'Build LXC GPU image done'
 
 # add gpu config to image
 lxc config set $CONTAINER gpu gpu
-lxc exec $CONTAINER -- echo $(nvidia-smi)
+lxc exec $CONTAINER -- echo "LXC GPU info"
+lxc exec $CONTAINER -- nvidia-smi
 
 # export image 
-echo "wait for export GPU image"
+echo "Create LXC GPU image template ${EXPORT_NAME}"
 lxc stop $CONTAINER
 lxc publish $CONTAINER --alias $EXPORT_NAME
-echo "export GPU image ${EXPORT_NAME} done"
+echo "CCreate LXC GPU image template: ${EXPORT_NAME} done"
